a77da13fb97e6cec7c901b11426a2195f334ad71
KlNon <y569034941@gmail.com>
2021-10-18 23:21:10 +0800

    改属性

--- src/CLASS_JAVA/Class_5/Pen.java
+++ src/CLASS_JAVA/Class_5/Pen.java
@@ -3,6 +3,7 @@ package CLASS_JAVA.Class_5;
 abstract public class Pen {
     private Color color;
 
+    abstract public void getSize();
     abstract public void painting();
     public String getColor() {
         return color.getColor();
