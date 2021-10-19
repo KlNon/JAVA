4f62107921fb466c2ccba8c7945e791760e2c46b
KlNon <y569034941@gmail.com>
2021-10-19 21:08:05 +0800

    注释

--- src/CLASS_JAVA/Class_8/Demo.java
+++ src/CLASS_JAVA/Class_8/Demo.java
@@ -2,7 +2,8 @@ package CLASS_JAVA.Class_8;
 
 import javax.swing.*;
 
-public class Demo extends JInternalFrame {
+public class Demo extends JInternalFrame {//JInternalFrame 支持在 JFrame 窗口内部显示一个完整的子窗口，并提供了许多本机窗体功能的轻量级对象，包括拖动、关闭、变成图标、调整大小、标题显示和支持菜单栏等。
+    //在实际使用中，通常将 JInternalFrame 添加到 JDesktopPane 中，由其来维护和显示 JInternalFrame。
     private static Demo demo;
     public Demo(String title, boolean resizable, boolean closable, boolean maximizable, boolean iconifiable){
         super(title, resizable, closable, maximizable, iconifiable);
--- src/CLASS_JAVA/Class_8/createFrame.java
+++ src/CLASS_JAVA/Class_8/createFrame.java
@@ -8,27 +8,27 @@ import java.beans.PropertyVetoException;
 
 public class createFrame extends JFrame implements ActionListener {
     private static Demo demo;
-    Container container=this.getContentPane();
+    Container container=this.getContentPane();//初始化一个容器
 
     public createFrame() {
         super("主窗体");
         container.setLayout(new BorderLayout());
         JButton button = new JButton("单击创建一个内部窗体");
-        button.addActionListener(this);//创建监听器
-        container.add(button, BorderLayout.SOUTH);
+        button.addActionListener(this);//当用户按下按钮时，将运行actionPerformed()中的程序
+        container.add(button, BorderLayout.SOUTH);//往容器中添加按钮
         this.setSize(new Dimension(500, 500));
         this.setVisible(true);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);//设置点击右上角X是否结束进程
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {//监听器反应
         demo = Demo.create("子窗体", true, true, true, true);
-        demo.setSize(new Dimension(300, 300));
+        demo.setSize(new Dimension(300, 300));//大小
         demo.setVisible(true);
-        JDesktopPane jDesktopPane = new JDesktopPane();
-        container.add(jDesktopPane);
-        jDesktopPane.add(demo);
+        JDesktopPane jDesktopPane = new JDesktopPane();//依附于上层组件而出现
+        container.add(jDesktopPane);//往容器中添加Pane
+        jDesktopPane.add(demo);//在子窗体中添加demo,即JInternalFrame
         try {
             demo.setSelected(true);
         } catch (PropertyVetoException e1) {
